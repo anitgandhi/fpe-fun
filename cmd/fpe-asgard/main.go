278ce577c7587df60fa7575b36985b9c5261e8e4
Anit Gandhi <anitgandhi@gmail.com>
2018-05-09 22:50:57 -0400

    use 16-byte array for key to show it doesn't escape

--- cmd/fpe-asgard/main.go
+++ cmd/fpe-asgard/main.go
@@ -7,22 +7,17 @@ import (
 
 	"github.com/capitalone/fpe/ff1"
 
-	"github.com/anitgandhi/aesguard"
+	"github.com/anitgandhi/asgard"
 )
 
 func main() {
 
 	// Key and tweak should be byte arrays. Put your key and tweak here.
-	// To make it easier for demo purposes, decode from a hex string here.
-	key, err := hex.DecodeString("2B7E151628AED2A6ABF7158809CF4F3C")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	var key = [16]byte{0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C}
 
-	// Create an AES block from the key, using aesguard to protect the key in memory
+	// Create an AES block from the key, using asgard to protect the key in memory
 	// This will be used to create the FF1 cipher
-	aesBlock, err := aesguard.NewCipher(key)
+	aesBlock, err := asgard.NewCipher(key[:])
 	if err != nil {
 		log.Println(err)
 		return
@@ -34,7 +29,7 @@ func main() {
 		return
 	}
 
-	// Create a new FF1 cipher "object" using the aesguard AES block we already made
+	// Create a new FF1 cipher "object" using the asgard AES block we already made
 	// 10 is the radix/base, and 8 is the max tweak length.
 	FF1, err := ff1.NewCipherWithBlock(aesBlock, 10, 8, tweak)
 	if err != nil {
@@ -61,7 +56,7 @@ func main() {
 		return
 	}
 
-	err = aesguard.DestroyCipher(aesBlock)
+	err = asgard.DestroyCipher(aesBlock)
 	if err != nil {
 		log.Println(err)
 		return
